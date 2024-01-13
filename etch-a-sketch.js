d7a3f369ad1c1a42f7d3be46a4b12ae62c8d8f07
Bang <bangchiem40@gmail.com>
2024-01-13 14:37:06 -0500

    added highlight funciton to grid of divs

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,15 +1,27 @@
 const gridsizewidth = 8;
 const gridsizeheight = 8;
+const squares = []
+
+function highcolorlight(e){
+    e.target.style.background = '#ADD8E6';
+}
+
 
 
 
 const etchSketchBoard = document.querySelector(".etch-a-sketchBOARD");
-for (let i = 0; i <= gridsizewidth; i++) {
-    const gridRow1Pixel = document.createElement("div");
-    etchSketchBoard.appendChild(gridRow1Pixel);
-    for (let j = 0; j <= gridsizeheight; j++) {
+for (let i = 0; i <= gridsizewidth-1; i++) {
+    const gridRow = document.createElement("div");
+    etchSketchBoard.appendChild(gridRow);
+    for (let j = 0; j <= gridsizeheight-1; j++) {
         const gridpixel = document.createElement("div");
-        gridRow1Pixel.appendChild(gridpixel);
+        gridRow.appendChild(gridpixel);
+        gridpixel.addEventListener("mouseover", highcolorlight)
+
         
     }
 }
+
+console.log(squares)
+
+//di.style.color
