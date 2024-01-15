4de20fadb55594f7de216ae155eee9204c7ea3f2
Bang <bangchiem40@gmail.com>
2024-01-15 10:38:06 -0500

    user can now input a desired grid size and generate a new board with that size. Also centered board horizontally.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,27 +1,53 @@
-const gridsizewidth = 8;
-const gridsizeheight = 8;
-const squares = []
+//create first board
+const first_gridsize = 0;
 
+    const etchSketchBoard = document.querySelector(".etch-a-sketchBOARD");
+    for (let i = 0; i <= first_gridsize; i++) {
+        const gridRow = document.createElement("div");
+        etchSketchBoard.appendChild(gridRow);
+        for (let j = 0; j <= first_gridsize; j++) {
+            const gridpixel = document.createElement("div");
+            gridRow.appendChild(gridpixel);
+            gridpixel.addEventListener("mouseover", highcolorlight)
+
+            
+        }
+    }
+
+//function to call when pixels are highlighted by cursor; changes pixel color
 function highcolorlight(e){
     e.target.style.background = '#ADD8E6';
 }
 
-
-
-
-const etchSketchBoard = document.querySelector(".etch-a-sketchBOARD");
-for (let i = 0; i <= gridsizewidth-1; i++) {
-    const gridRow = document.createElement("div");
-    etchSketchBoard.appendChild(gridRow);
-    for (let j = 0; j <= gridsizeheight-1; j++) {
-        const gridpixel = document.createElement("div");
-        gridRow.appendChild(gridpixel);
-        gridpixel.addEventListener("mouseover", highcolorlight)
-
-        
+//clear board
+function resetboard(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
     }
 }
 
-console.log(squares)
+//
+function createboard(form){
+    const etchSketchBoard = document.querySelector(".etch-a-sketchBOARD");
 
-//di.style.color
+    //reset board
+    resetboard(etchSketchBoard)
+
+    //create board with user inputted size
+    const gridsize = form.inputbox.value - 1;
+
+    if (gridsize > 100){
+        console.log("too big")
+    }
+    for (let i = 0; i <= gridsize; i++) {
+        const gridRow = document.createElement("div");
+        etchSketchBoard.appendChild(gridRow);
+        for (let j = 0; j <= gridsize; j++) {
+            const gridpixel = document.createElement("div");
+            gridRow.appendChild(gridpixel);
+            gridpixel.addEventListener("mouseover", highcolorlight)
+
+            
+        }
+    }
+}
